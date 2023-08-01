13f26abc77996ff0720e251b57dba982a5e9bcb6
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2023-08-01 10:30:58 +0700

    Revert "test profil"
    This reverts commit 518b135162ca466abea2b522886d16ff2d85ff6e.

---
+++ Test Suites/Marquee/TS_MarqueeID.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_MarqueeID</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>87c3c922-4238-4cc9-86c4-2f0849fa4707</testSuiteGuid>
+   <testCaseLink>
+      <guid>6729ab3f-7a87-482d-a805-f56db8062839</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC009_MarqueeID</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
