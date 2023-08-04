e1082631017537cae16a07aca9f029ac15782f11
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2023-08-04 13:45:02 +0700

    test beranda

--- Test Suites/TS_Beranda.ts
+++ Test Suites/TS_Beranda.ts
@@ -15,42 +15,42 @@
       <guid>524cb6a5-c214-4653-9670-94b9805b8fcd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Beranda/TC001_CaroselHomeID</testCaseId>
+      <testCaseId>Test Cases/Beranda/TC001_CaroselHome</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>8815dc19-31b1-4d7c-85ae-eac8ae540532</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Beranda/TC003_VideoID</testCaseId>
+      <testCaseId>Test Cases/Beranda/TC003_Video</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>a7ab7628-b20b-406d-9d63-231434ab920d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Beranda/TC004_MarqueeID</testCaseId>
+      <testCaseId>Test Cases/Beranda/TC004_Marquee</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>1fa8ee79-5eb6-4446-a8c7-eb5e1662ab75</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Beranda/TC006_LaporannID</testCaseId>
+      <testCaseId>Test Cases/Beranda/TC006_Laporan</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>c63f4103-54ab-44dd-9b58-51bcca33c617</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Beranda/TC005_OwlHomeID</testCaseId>
+      <testCaseId>Test Cases/Beranda/TC005_OwlHome</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>1eada414-3f0f-4823-89ce-f59f77fcf441</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Beranda/TC002_LatestNewsID</testCaseId>
+      <testCaseId>Test Cases/Beranda/TC002_LatestNews</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
